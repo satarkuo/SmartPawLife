68a1ba99d33864819bae6e78a3d1ae9800abd67d
SatarKuo <satarkuo@gmail.com>
2025-03-12 16:43:32 +0800

    edit env

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_APP_BASE_PATH || '/',
+  base: '/SmartPawLife/',
   plugins: [react()],
 })
